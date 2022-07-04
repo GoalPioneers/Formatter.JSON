ea3ad5462c30234978cd871e0c4b103cfa8a99a8
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-07-04 14:06:05 +0200

    staging

--- code/src/GoalPioneers/Formats/JSON/JSONExporter.java
+++ code/src/GoalPioneers/Formats/JSON/JSONExporter.java
@@ -1,7 +1,17 @@
 package GoalPioneers.Formats.JSON;
 
+import GoalPioneers.Formats.Templates.GraphFiles.GraphExporterFacade;
+
+
+/**
+ * 
+ */
 public class JSONExporter 
+        implements GraphExporterFacade
 {
+    /**
+     * 
+     */
     public JSONExporter()
     {
         
--- code/src/GoalPioneers/Formats/JSON/JSONImporter.java
+++ code/src/GoalPioneers/Formats/JSON/JSONImporter.java
@@ -1,10 +1,13 @@
 package GoalPioneers.Formats.JSON;
 
+import GoalPioneers.Formats.Templates.GraphFiles.GraphImporterFacade;
+
 
 /**
  * 
  */
 public class JSONImporter 
+        implements GraphImporterFacade
 {
     // Constructor
     public JSONImporter()
